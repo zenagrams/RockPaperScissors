93ab6a5f434a802268c85057e7b98e9bf1c9cdf2
zenagrams <110210132+zenagrams@users.noreply.github.com>
2023-01-05 20:13:37 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//if click rock 
+
+//show for user
+
+//generate computer choice
+
+//show the result of the winner
+
+//if click paper
+
+//show for user
+
+//generate computer choice
+
+//show result of the winner
+
+//if click scissors
+
+//show for user
+
+//generate computer choice
+
+//show the result of winner
